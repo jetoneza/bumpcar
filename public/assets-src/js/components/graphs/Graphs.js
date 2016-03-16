3d808400844b78918a05f6b15b108ad7cfd69025
Jethro Ordaneza <jet.oneza@gmail.com>
2016-03-16 20:47:14 +0800

    Added datepicker on graphs

--- public/assets-src/js/components/graphs/Graphs.js
+++ public/assets-src/js/components/graphs/Graphs.js
@@ -5,6 +5,8 @@ import BarGraph from './BarGraph';
 import ViolationActions from '../../actions/ViolationActions';
 import Stores from '../../stores';
 import PieGraph from './PieGraph';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
 /**
  * Graph Component
@@ -15,7 +17,9 @@ class Graphs extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: null
+      count: null,
+      startDate: moment(),
+      endDate: moment().add(7, 'days')
     };
   }
 
@@ -45,6 +49,14 @@ class Graphs extends React.Component {
     );
   }
 
+  _handleStartDateChange(startDate) {
+    this.setState({startDate})
+  }
+
+  _handleEndDateChange(endDate) {
+    this.setState({endDate});
+  }
+
   /**
    * Returns the component markup
    * @returns {XML}
@@ -55,6 +67,18 @@ class Graphs extends React.Component {
     return (
         <div className="ui center aligned container graphs-component">
           <h1>Graphs</h1>
+          <div className="ui grid form dates">
+            <div className="eight wide column">
+              <label>Start Date</label>
+              <DatePicker ref="startDate" className="start-date" selected={this.state.startDate}
+                          onChange={this._handleStartDateChange.bind(this)}/>
+            </div>
+            <div className="eight wide column">
+              <label>End Date</label>
+              <DatePicker ref="endDate" className="end-date" selected={this.state.endDate}
+                          onChange={this._handleEndDateChange.bind(this)}/>
+            </div>
+          </div>
           {graphs}
         </div>
     );
